e6407ca7a53009a0d310bbac0c28ae5128939ba7
zhoujieluntai <2682831582@qq.com>
2021-06-04 13:23:40 +0800

    article部分

---
+++ assets/js/article/art_cate.js
@@ -0,0 +1,54 @@
+$(function() {
+    var layer = layui.layer
+    var form = layui.form
+
+    initArtCateList()
+
+    // 获取文章分类的列表
+    function initArtCateList() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates',
+            success: function(res) {
+                var htmlStr = template('tpl-table', res)
+                $('tbody').html(htmlStr)
+            }
+        })
+    }
+
+    // 为添加类别按钮绑定点击事件
+    var indexAdd = null
+    $('#btnAddCate').on('click', function() {
+        indexAdd = layer.open({
+            type: 1,
+            area: ['500px', '250px'],
+            title: '添加文章分类',
+            content: $('#dialog-add').html()
+        })
+    })
+
+    // 通过代理的形式，为 form-add 表单绑定 submit 事件
+    $('body').on('submit', '#form-add', function(e) {
+        e.preventDefault()
+        $.ajax({
+            method: 'POST',
+            url: '/my/article/addcates',
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg('新增分类失败！')
+                }
+                initArtCateList()
+                layer.msg('新增分类成功！')
+                    // 根据索引，关闭对应的弹出层
+                layer.close(indexAdd)
+            }
+        })
+    })
+
+    // 通过代理的形式，为 btn-edit 按钮绑定点击事件
+
+    // 通过代理的形式，为修改分类的表单绑定 submit 事件
+
+    // 通过代理的形式，为删除按钮绑定点击事件
+})
